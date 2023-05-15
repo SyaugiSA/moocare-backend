4e758fe01d40c4a5afe7574538d3c6873ebc3150
SyaugiSA <syaugiamar9@gmail.com>
2023-05-15 14:59:32 +0700

    update dataset

--- model/dataset.model.js
+++ model/dataset.model.js
@@ -2,9 +2,6 @@ const { Schema, model } = require("mongoose");
 
 const DatasetSchema = new Schema({
   heartRate: { type: Number, default: 0 },
-  gyroX: {type:Number, default:0},
-  gyroY: {type:Number, default:0},
-  gyroZ: {type:Number, default:0},
 });
 
 const DatasetModel = model("Dataset", DatasetSchema);
--- routes/dataset.route.js
+++ routes/dataset.route.js
@@ -3,13 +3,16 @@ const router = require("express").Router();
 
 router.get("/", async (req, res) => {
   const data = await DatasetModel.find();
-  res.status(200).json({ status: true, message: "Daftar Data", data });
+  res.status(200).json({ status: true, message: "Daftar Data Training", data });
 });
 
 router.post("/", async (req, res) => {
-  const { heartRate, gyroX, gyroY, gyroZ } = req.body;
-  const data = new DatasetModel({ heartRate, gyroX, gyroY, gyroZ });
+  const { heartRate } = req.body;
+  const hr = Number(heartRate);
+  const data = new DatasetModel({ heartRate: hr });
   data.save();
+
+  console.log({ heartRate: hr });
 
   res
     .status(201)
