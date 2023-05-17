bc648b4a6dbda27ca057136abdf119f55546951f
SyaugiSA <syaugiamar9@gmail.com>
2023-05-17 21:05:51 +0700

    add click average

--- routes/click.route.js
+++ routes/click.route.js
@@ -19,4 +19,28 @@ router.get("/", async (req, res) => {
   res.status(200).json({ data });
 });
 
+router.get("/average", async (req, res) => {
+  const buttonList = await ClickModel.distinct("button");
+  let data = [];
+
+  buttonList.map(async (list) => {
+    const dataList = await ClickModel.find({ button: list });
+    let time = 0;
+    let name = 0;
+
+    dataList.map((val) => {
+      time += val.time;
+      name = val.button;
+    });
+    const average = time / dataList.length;
+
+    data.push({ responden: dataList.length, name, average });
+  });
+
+  setTimeout(() => {
+    console.log({ data });
+    res.status(200).json({ data });
+  }, 1000);
+});
+
 module.exports = router;
